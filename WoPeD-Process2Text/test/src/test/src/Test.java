32fab03b89a716aa1455dca3e6ee0e81324f5e49
thomsBall <thomas.ballauer@gmail.com>
2020-05-25 08:20:29 +0200

    Equals Methode in der Test Class geändert

--- WoPeD-Process2Text/test/src/test/src/Test.java
+++ WoPeD-Process2Text/test/src/test/src/Test.java
@@ -1,6 +1,7 @@
 package test.src;
 
 import org.woped.p2t.textGenerator.TextGenerator;
+import org.woped.p2t.textPlanning.TextPlanner;
 
 import java.nio.file.Path;
 import java.util.*;
@@ -23,36 +24,36 @@ public class Test {
     private static final SortedMap<String, List<String>> tests = new TreeMap<>();
 
     static {
-   //      Key = Dateiname, Value = Erwartetes Ergebnis
+        //      Key = Dateiname, Value = Erwartetes Ergebnis
         tests.put("LoanApplication-Resources.pnml", Arrays.asList(
                 "<phrase ids=\"t17\"> The process begins, when it is registered. </phrase>",
-                         "<phrase ids=\"t1_op_1\"> Then, the process is split into 3 parallel branches: </phrase>",
-                         "<phrase ids=\"t5\"> The Clerk checks form. </phrase>",
-                         "<phrase ids=\"t4\"> The Clerk checks the history. </phrase>",
-                         "<phrase ids=\"t3\"> The Manager checks the funds. </phrase>",
-                         "<phrase ids=\"t1_op_1\"> Once all 3 branches were executed, it is decided. </phrase>",
-                         "<phrase ids=\"p12\"> Afterwards, one of the following branches is executed: </phrase>",
-                         "<phrase ids=\"t12_op_1\"> The Manager decide:s the granted. </phrase>",
-                         "<phrase ids=\"t13\"> Subsequently, the Secretary sends the approval. </phrase>",
-                         "<phrase ids=\"t14_op_2\"> Then, it is archived. </phrase>",
-                         "<phrase ids=\"t12_op_2\"> The Manager decide:s the rejected. </phrase>",
-                         "<phrase ids=\"t15\"> Afterwards, the Secretary sends the rejection. </phrase>",
-                         "<phrase ids=\"t14_op_1\"> Subsequently, it is archived. </phrase>"
-                ));
+                "<phrase ids=\"t1_op_1\"> Then, the process is split into 3 parallel branches: </phrase>",
+                "<phrase ids=\"t5\"> The Clerk checks form. </phrase>",
+                "<phrase ids=\"t4\"> The Clerk checks the history. </phrase>",
+                "<phrase ids=\"t3\"> The Manager checks the funds. </phrase>",
+                "<phrase ids=\"t1_op_1\"> Once all 3 branches were executed, it is decided. </phrase>",
+                "<phrase ids=\"p12\"> Afterwards, one of the following branches is executed: </phrase>",
+                "<phrase ids=\"t12_op_1\"> The Manager decide:s the granted. </phrase>",
+                "<phrase ids=\"t13\"> Subsequently, the Secretary sends the approval. </phrase>",
+                "<phrase ids=\"t14_op_2\"> Then, it is archived. </phrase>",
+                "<phrase ids=\"t12_op_2\"> The Manager decide:s the rejected. </phrase>",
+                "<phrase ids=\"t15\"> Afterwards, the Secretary sends the rejection. </phrase>",
+                "<phrase ids=\"t14_op_1\"> Subsequently, it is archived. </phrase>"
+        ));
         tests.put("LoanApplication.pnml", Arrays.asList(
                 "<phrase ids=\"t17\"> The process begins, when it is registered. </phrase>",
-                        "<phrase ids=\"null, t1_op_1\"> Then, the process is split into 3 parallel branches: </phrase>",
-                        "<phrase ids=\"t4\"> The history is checked. </phrase>,",
-                        "<phrase ids=\"t3\"> The funds is checked. </phrase>",
-                        "<phrase ids=\"t5\"> Form is checked. </phrase>",
-                        "<phrase ids=\"t1_op_1\"> Once all 3 branches were executed, it is decided. </phrase>",
-                        "<phrase ids=\"p12\"> Afterwards, one of the following branches is executed: </phrase>",
-                        "<phrase ids=\"t12_op_1\"> The granted is decide:ed. </phrase>",
-                        "<phrase ids=\"t13\"> Subsequently, the approval is sent. </phrase>",
-                        "<phrase ids=\"t14_op_2\"> Then, it is archived. </phrase>",
-                        "<phrase ids=\"t12_op_2\"> The rejected is decide:ed. </phrase>",
-                        "<phrase ids=\"t15\"> Afterwards, the rejection is sent. </phrase>",
-                        "<phrase ids=\"t14_op_1\"> Subsequently, it is archived. </phrase>"
+                "<phrase ids=\"null, t1_op_1\"> Then, the process is split into 3 parallel branches: </phrase>",
+                "<phrase ids=\"t4\"> The history is checked. </phrase>,",
+                "<phrase ids=\"t3\"> The funds is checked. </phrase>",
+                "<phrase ids=\"t5\"> Form is checked. </phrase>",
+                "<phrase ids=\"t1_op_1\"> Once all 3 branches were executed, it is decided. </phrase>",
+                "<phrase ids=\"p12\"> Afterwards, one of the following branches is executed: </phrase>",
+                "<phrase ids=\"t12_op_1\"> The granted is decide:ed. </phrase>",
+                "<phrase ids=\"t13\"> Subsequently, the approval is sent. </phrase>",
+                "<phrase ids=\"t14_op_2\"> Then, it is archived. </phrase>",
+                "<phrase ids=\"t12_op_2\"> The rejected is decide:ed. </phrase>",
+                "<phrase ids=\"t15\"> Afterwards, the rejection is sent. </phrase>",
+                "<phrase ids=\"t14_op_1\"> Subsequently, it is archived. </phrase>"
         ));
         tests.put("AND - (PetriNet Logik).pnml", Arrays.asList(
                 "<phrase ids=\"t3\"> The process begins, when a customer database is updated. </phrase><phrase ids=\"t2\"> In concurrency to the latter step, the receipt is printed. </phrase>",
@@ -89,7 +90,7 @@ public class Test {
                 "<phrase ids=\"t1\"> The process begins, when a text is read. </phrase><phrase ids=\"t3\"> Then, the text is printed. </phrase>"
         ));
         tests.put("XOR (PetriNet Logik).pnml", Arrays.asList(
-                "<phrase ids=\"t5\"> The process begins, when a payment is checked. </phrase><phrase ids=\"p8\"> Then, one of the following branches is executed: </phrase><phrase ids=\"t7\"> The t7 is conducted. </phrase><phrase ids=\"t9\"> Afterwards, the customer is informed. </phrase><phrase ids=\"t6\"> The t6 is conducted. </phrase><phrase ids=\"t8\"> Subsequently, the customer is informed. </phrase>"
+                "<phrase ids=\"t5\"> The process begins, when the Manager checks a payment. </phrase><phrase ids=\"p8\"> Then, one of the following branches is executed: </phrase><phrase ids=\"t7\"> The t7 is conducted. </phrase><phrase ids=\"t9\"> Afterwards, the customer is informed. </phrase><phrase ids=\"t6\"> The t6 is conducted. </phrase><phrase ids=\"t8\"> Subsequently, the customer is informed. </phrase>"
         ));
         tests.put("XOR-SplitnJoin.pnml", Arrays.asList(
                 "<phrase ids=\"null\"> The process begins, when it is checked for the recepies. </phrase><phrase ids=\"p1\"> Then, one of the following branches is executed: </phrase><phrase ids=\"t1_op_2\"> It is checked for the recepies: veggie recepie found. </phrase><phrase ids=\"t2\"> Afterwards, the needed ingredients is gotten. </phrase><phrase ids=\"t4_op_1\"> Subsequently, the meal is prepared. </phrase><phrase ids=\"t1_op_1\"> It is checked for the recepies: meat recepie found. </phrase><phrase ids=\"t3\"> Then, the needed ingredients is gotten. </phrase><phrase ids=\"t4_op_2\"> Afterwards, the meal is prepared. </phrase>",
@@ -113,10 +114,10 @@ public class Test {
             String result;
 
             try {
-                TextGenerator textGenerator = new TextGenerator(new java.io.File( "." ).getCanonicalPath()+ "/WoPeD-Process2Text/bin");
-                System.out.print(test.getKey()+ ": ");
+                TextGenerator textGenerator = new TextGenerator(new java.io.File(".").getCanonicalPath() + "/WoPeD-Process2Text/bin");
+                System.out.print(test.getKey() + ": ");
                 String content = new String(Files.readAllBytes(Paths.get(BASE_PATH + test.getKey())));
-                File f = new File (BASE_PATH + test.getKey());
+                File f = new File(BASE_PATH + test.getKey());
                 System.out.println(f.getAbsolutePath());
                 result = textGenerator.toText(content);
                 result = removeNewLinesAndSurroundingSpaces(result);
@@ -130,7 +131,7 @@ public class Test {
 
             if (result.isEmpty()) {
                 System.out.println(TEST_FAILED + " failed with no result.");
-                failed++;
+                error++;
                 continue;
             }
 
@@ -140,7 +141,6 @@ public class Test {
                 failed++;
                 continue;
             }
-
             successful++;
             System.out.println(TEST_SUCCESS + " succeeded.");
         }
@@ -166,13 +166,58 @@ public class Test {
         }
     }
 
+
     private static boolean equals(String result, List<String> control) {
         for (String c : control) {
-            if (result.equals(removeNewLinesAndSurroundingSpaces(c))) {
-                return true;
+            //if (result.equals(removeNewLinesAndSurroundingSpaces(c))) {
+
+                String[] splitResult = result.split("</phrase>");
+                String[] splitControll = c.split("</phrase>");
+                int i = 0;
+                /*for (String f : splitResult) {
+                    splitResult[i] = splitResult[i] + "</phrase>";
+                    i++;
+                }*/
+                int numberOfResultPhrases = splitResult.length;
+                boolean prüfung = true;
+                int levent = 0;
+                int truePattern = 0;
+                int falsePattern = 0;
+                for (int j = 0; j < splitResult.length-1; j++) {
+                    prüfung = KMPSearch(splitResult[j], splitControll[j]);
+                    levent = diff(splitControll[j],splitResult[j]);
+                    System.out.println("///////////////////////////  Prüfung " + prüfung + ": " + j);
+                    System.out.println("///////////////////////////  Leven " + levent + ": " + j);
+                    if(prüfung){
+                        truePattern++;
+                    } else{
+                        if (levent <20){
+                            truePattern++;
+                        } else {
+                            falsePattern++;
+                        }
+                    }
+                }
+                System.out.println("/////////////////////////// + Numberof ResultPhrases   " + numberOfResultPhrases);
+                System.out.println("/////////////////////////// + Numberof True Pattern   " + truePattern);
+                System.out.println("/////////////////////////// + Numberof False Pattern   " + falsePattern);
+            System.out.println("/////////////////////////// + Percentage   " + percentageCalculator(numberOfResultPhrases, truePattern));
+
+            if(percentageCalculator(numberOfResultPhrases, truePattern)>= 50.0){
+                    return  true;
+                } else {
+                    return false;
+                }
+               /* if (result.equals(removeNewLinesAndSurroundingSpaces(c))) {
+                    return true;
+                }*/
             }
-        }
+      //  }
         return false;
+    }
+
+    private static double percentageCalculator(int numberOfResultPhrases, int truePattern) {
+        return  (truePattern*100)/numberOfResultPhrases;
     }
 
     private static String removeNewLinesAndSurroundingSpaces(String input) {
@@ -193,5 +238,110 @@ public class Test {
             System.out.println("\t\t\t- " + s);
         }
         System.out.println("\tBut got:  " + actual);
+    }
+
+    public static int diff(String orig, String eing) {
+        int matrix[][] = new int[orig.length() + 1][eing.length() + 1];
+        for (int i = 0; i < orig.length() + 1; i++) {
+            matrix[i][0] = i;
+        }
+        for (int i = 0; i < eing.length() + 1; i++) {
+            matrix[0][i] = i;
+        }
+        for (int a = 1; a < orig.length() + 1; a++) {
+            for (int b = 1; b < eing.length() + 1; b++) {
+                int right = 0;
+                if (orig.charAt(a - 1) != eing.charAt(b - 1)) {
+                    right = 1;
+                }
+                int mini = matrix[a - 1][b] + 1;
+                if (matrix[a][b - 1] + 1 < mini) {
+                    mini = matrix[a][b - 1] + 1;
+                }
+                if (matrix[a - 1][b - 1] + right < mini) {
+                    mini = matrix[a - 1][b - 1] + right;
+                }
+                matrix[a][b] = mini;
+            }
+        }
+        return matrix[orig.length()][eing.length()];
+    }
+
+    public static boolean KMPSearch(String pat, String txt) {
+        int M = pat.length();
+        int N = txt.length();
+        System.out.println("Pattern///////" + pat);
+        System.out.println("Text//////////" + txt);
+
+        // create lps[] that will hold the longest
+        // prefix suffix values for pattern
+        int lps[] = new int[M];
+        int j = 0; // index for pat[]
+        int foundPatternNumb = 0;
+
+        // Preprocess the pattern (calculate lps[]
+        // array)
+        computeLPSArray(pat, M, lps);
+        boolean wert = false;
+        int i = 0; // index for txt[]
+        while (i < N) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                wert = true;
+                foundPatternNumb++;
+
+                System.out.println(wert);
+                System.out.println("Found pattern "
+                        + "at index " + (i - j));
+                j = lps[j - 1];
+            }
+
+
+            // mismatch after j matches
+            else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+        return wert;
+    }
+
+
+    public static void computeLPSArray(String pat, int M, int lps[]) {
+        // length of the previous longest prefix suffix
+        int len = 0;
+        int i = 1;
+        lps[0] = 0; // lps[0] is always 0
+
+        // the loop calculates lps[i] for i = 1 to M-1
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else // (pat[i] != pat[len])
+            {
+                // This is tricky. Consider the example.
+                // AAACAAAA and i = 7. The idea is similar
+                // to search step.
+                if (len != 0) {
+                    len = lps[len - 1];
+
+                    // Also, note that we do not increment
+                    // i here
+                } else // if (len == 0)
+                {
+                    lps[i] = len;
+                    i++;
+                }
+            }
+        }
     }
 }
