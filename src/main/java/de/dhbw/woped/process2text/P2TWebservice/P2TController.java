49eaa513db461f8b9c8765f1353fe41f0375b49f
Nils <nils.leger@gmail.com>
2021-06-14 18:48:12 +0200

    Add simple UI to test P2T

--- src/main/java/de/dhbw/woped/process2text/P2TWebservice/P2TController.java
+++ src/main/java/de/dhbw/woped/process2text/P2TWebservice/P2TController.java
@@ -11,18 +11,29 @@ public class P2TController extends Thread {
     public static final int MAX_INPUT_LENGTH=15000;//Reject any Request larger than this
 
     public String generateText(String text) {
-        this.text = text;
+        this.text = prepareText(text);
         String output = "";
         TextGenerator tg = new TextGenerator();
 
         try {
-            output = tg.toText(text, true);
+            output = tg.toText(this.text, true);
 
         }
         catch (Exception e) {
             e.printStackTrace();
         }
         return output;
+    }
+
+    /**
+     * In case the pnml in send via an html form, it starts with the name of the input form.
+     * For further processing, this part must be removed.
+     * @param pnml the pnml
+     * @return a valid xml
+     */
+    private String prepareText(String pnml) {
+        pnml = pnml.trim();
+        return pnml.startsWith("=") ? pnml.substring(1) : pnml;
     }
 
     public String getText() {
