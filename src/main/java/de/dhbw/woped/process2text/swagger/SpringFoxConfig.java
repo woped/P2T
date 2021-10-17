2db5c13d6aca322bebfa0b98b9b86e989f2c5fe0
ChelloX <43784048+ChelloX@users.noreply.github.com>
2021-10-17 11:32:55 +0200

    Fixed local jar-Libs

--- src/main/java/de/dhbw/woped/process2text/swagger/SpringFoxConfig.java
+++ src/main/java/de/dhbw/woped/process2text/swagger/SpringFoxConfig.java
@@ -2,6 +2,8 @@ package de.dhbw.woped.process2text.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -13,23 +15,19 @@ import java.util.Collections;
 
 @Configuration
 public class SpringFoxConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+				.paths(PathSelectors.any())
+				.build().apiInfo(apiInfo());
+	}
 
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "WoPeD - Process2Text Webservice",
-                "This webservice can be used to translate a process model into a human readable text.",
-                "3.8.0",
-                null,
-                new Contact("Prof. Dr. Thomas Freytag", "https://woped.dhbw-karlsruhe.de/", "thomas.freytag@dhbw-karlsruhe.de"),
-                "License", "https://github.com/tfreytag/P2T/blob/master/LICENSE.md", Collections.emptyList());
-    }
+	private ApiInfo apiInfo() {
+		return new ApiInfo("WoPeD - Process2Text Webservice",
+				"This webservice can be used to translate a process model into a human readable text.", "3.8.0", null,
+				new Contact("Prof. Dr. Thomas Freytag", "https://woped.dhbw-karlsruhe.de/",
+						"thomas.freytag@dhbw-karlsruhe.de"),
+				"License", "https://github.com/tfreytag/P2T/blob/master/LICENSE.md", Collections.emptyList());
+	}
 }
