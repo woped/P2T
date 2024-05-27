c43b4e0976f1e4276d15660e08414a5b485a0336
Christopher Caspari <caspari.christop.a21@student.dhbw-Karlsruhe.de>
2024-05-27 12:00:38 +0200

    fixed display Bug at the frontend with Annotation in P2TController
    added a @CrossOrigin annotation to reference to all domains - fixxing the missing Request Methods with this annotation
    
    Co-Authored-By: JNIKER <116815909+JNIKER@users.noreply.github.com>
    Co-Authored-By: nilschi <90916219+nilschi@users.noreply.github.com>
    Co-Authored-By: JeremiasDecker <112878328+JeremiasDecker@users.noreply.github.com>

--- src/main/java/de/dhbw/woped/process2text/controller/P2TController.java
+++ src/main/java/de/dhbw/woped/process2text/controller/P2TController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @Slf4j
 public class P2TController {
