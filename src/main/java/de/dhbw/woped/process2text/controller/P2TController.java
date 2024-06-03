6b9557ca477feeaad5539c484398184943f8e160
Christopher Caspari <caspari.christop.a21@student.dhbw-Karlsruhe.de>
2024-06-03 16:14:41 +0200

    WebConfig created for CORS
    created a WebConfig class for the ACAO, to define the Crossorigin URLs
    
    Co-Authored-By: JNIKER <116815909+JNIKER@users.noreply.github.com>
    Co-Authored-By: JeremiasDecker <112878328+JeremiasDecker@users.noreply.github.com>
    Co-Authored-By: Avvy10 <126588211+Avvy10@users.noreply.github.com>
    Co-Authored-By: nilschi <90916219+nilschi@users.noreply.github.com>

--- src/main/java/de/dhbw/woped/process2text/controller/P2TController.java
+++ src/main/java/de/dhbw/woped/process2text/controller/P2TController.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @Slf4j
 public class P2TController {
