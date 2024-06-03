10cb96dea40030becce14c44686bafc9216d52d2
Marcel2511 <marcel.wollbaum@t-online.de>
2024-06-03 18:57:33 +0200

    Added Dependy, refactored Transformer Identification
    Added a needed dependency to figure out wheter it's a BPMN or PNML File and changed the detection from XML to HTML file

--- src/main/java/de/dhbw/woped/process2text/service/TransformerService.java
+++ src/main/java/de/dhbw/woped/process2text/service/TransformerService.java
@@ -5,14 +5,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Service
@@ -39,18 +35,17 @@ public class TransformerService {
                 .block();
     }
 
-
     /* the method analyzes a given XML content in the form of a string and determines the file format based
-    on the root element of the XML document */
-    public String checkForBPMNorPNML(String file) throws ParserConfigurationException, IOException, SAXException {
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = dbFactory.newDocumentBuilder();
-        Document document = documentBuilder.parse(new ByteArrayInputStream(file.getBytes()));
-        document.getDocumentElement().normalize();
+    on the root element of the HTML document */
+    public String checkForBPMNorPNML(String file) throws IOException {
+        // Parse the HTML content
+        Document document = Jsoup.parse(file);
 
-        Element rootElement = document.getDocumentElement();
-        String rootTag = rootElement.getTagName();
+        // Get the root element
+        Element rootElement = document.child(0);
+        String rootTag = rootElement.tagName();
 
+        // Check the root element tag name
         if ("pnml".equalsIgnoreCase(rootTag)) {
             return "PNML";
         } else if ("definitions".equalsIgnoreCase(rootTag)) {
