d7d38161525fd11f7fca2d3f1411f080c0aad78b
TimU02 <tim.uhlemann@hotmail.de>
2024-07-05 12:26:22 +0200

    Add JavaDoc for TransformerService

--- src/main/java/de/dhbw/woped/process2text/service/TransformerService.java
+++ src/main/java/de/dhbw/woped/process2text/service/TransformerService.java
@@ -10,16 +10,31 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
+/**
+ * Service class to handle the transformer call for the transformation from PNML to BPMN
+ * and detect which format was provided.
+ */
 @Service
 public class TransformerService {
 
   private final WebClient webClient;
 
+  /**
+   * Constructor to create a new WebClient instance containing the URL of the transformer API.
+   */
   public TransformerService() {
     this.webClient =
         WebClient.builder().baseUrl("https://europe-west3-woped-422510.cloudfunctions.net").build();
   }
 
+  /**
+   * Calls the transformer API with the provided direction and PNML content using the webClient,
+   * and retrieves the transformed BPMN content.
+   *
+   * @param direction The direction of the transformation (e.g., "pnmltobpmn").
+   * @param pnmlXml The PNML content to be transformed.
+   * @return The transformed content.
+   */
   public String transform(String direction, String pnmlXml) {
     String endpoint =
         UriComponentsBuilder.fromUriString("/transform")
@@ -36,8 +51,13 @@ public class TransformerService {
         .block();
   }
 
-  /* the method analyzes a given XML content in the form of a string and determines the file format based
-  on the root element of the XML document */
+ /**
+   * Checks the root element of the provided diagram file (XML String)
+   * to determine if it is PNML or BPMN.
+   *
+   * @param file The diagram file to be checked.
+   * @return The type of the diagram ("PNML", "BPMN", or "Unknown").
+   */
   public String checkForBPMNorPNML(String file) {
     // Parse the HTML content
     Document document = Jsoup.parse(file, "", Parser.xmlParser());
